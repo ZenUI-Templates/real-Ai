009575640426c4e3d1f50ce7ea88ed5c3143c048
kopildas <102140363+kopildas@users.noreply.github.com>
2024-03-16 17:37:40 +0600

    feat: custom color added on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'custom-orange': '#FF8057',
+      },
+      colors: {
+        'custom-orange': '#FF8057',
+        'text-color2': '#3B404A',
+      }
+    },
   },
   plugins: [],
 }
