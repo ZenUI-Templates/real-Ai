9dcc7b7ea3e855fb5d2026274adb73dfc48c22cf
kopildas <102140363+kopildas@users.noreply.github.com>
2024-03-17 11:00:26 +0600

    feat: forth section added

---
+++ src/components/Home/Forth_Section.jsx
@@ -0,0 +1,199 @@
+import React from "react";
+import { FaPlay } from "react-icons/fa";
+import { MdKeyboardArrowRight } from "react-icons/md";
+
+export const Forth_Section = () => {
+  return (
+    <div className="w-full h-full px-10 bg-red-00 flex flex-col items-center justify-center mb-40 -mt-10 md:mt-10">
+      <p className="text-3xl md:text-5xl font-bold mb-5">Get creative with Murf Studio</p>
+
+      <div className="h-96 w-full flex flex-col lg:flex-row mt-36 md:mt-5 lg:mt-0 bg-red-00  lg:gap-16">
+        <div className="lg:w-[60%] h-full bg-green-00 flex flex-col lg:flex-row items-center justify-center md:-ml-24 lg:ml-0 md:mt-20 lg:mt-0">
+          <div className="w-60 md:w-80 lg:w-96 -mt-44 md:mt-0 bg-white shadow-md flex flex-col p-3 rounded-md">
+            <div>
+              <img src="https://i.ibb.co/KW1zWGH/Group-16.png" alt="" />
+            </div>
+            <div className="mt-3">
+              <img src="https://i.ibb.co/bJtND8q/Group-14.png" alt="" />
+            </div>
+          </div>
+          <div className="w-72 md:ml-80 absolute h-72 mt-60 md:mt-0 bg-gray-400 rounded-2xl p-2">
+            <div className="w-full h-full bg-white rounded-2xl flex flex-wrap p-2 gap-2">
+              <div className="w-20 h-20 rounded-2xl bg-red-300 bg-cover overflow-hidden">
+                <img
+                  src="https://i.ibb.co/QD9pGwY/Rectangle-140.png"
+                  alt=""
+                  className="hover:scale-110 duration-500 transition ease-in-out"
+                />
+              </div>
+              <div className="w-20 h-20 rounded-2xl bg-red-300 bg-cover overflow-hidden">
+                <img
+                  src="https://i.ibb.co/F7h44Bm/Rectangle-141.png"
+                  alt=""
+                  className="hover:scale-110 duration-500 transition ease-in-out"
+                />
+              </div>
+              <div className="w-20 h-20 rounded-2xl bg-red-300 bg-cover overflow-hidden">
+                <img
+                  src="https://i.ibb.co/MfXrzDH/Rectangle-142.png"
+                  alt=""
+                  className="hover:scale-110 duration-500 transition ease-in-out"
+                />
+              </div>
+              <div className="w-20 h-20 rounded-2xl bg-red-300 bg-cover overflow-hidden">
+                <img
+                  src="https://i.ibb.co/rcK3k5c/Rectangle-143.png"
+                  alt=""
+                  className="hover:scale-110 duration-500 transition ease-in-out"
+                />
+              </div>
+              <div className="w-20 h-20 rounded-2xl bg-red-300 bg-cover overflow-hidden">
+                <img
+                  src="https://i.ibb.co/bghjnk5/Rectangle-144.png"
+                  alt=""
+                  className="hover:scale-110 duration-500 transition ease-in-out"
+                />
+              </div>
+              <div className="w-20 h-20 rounded-2xl bg-red-300 bg-cover overflow-hidden">
+                <img
+                  src="https://i.ibb.co/TBpfwT8/Rectangle-145.png"
+                  alt=""
+                  className="hover:scale-110 duration-500 transition ease-in-out"
+                />
+              </div>
+              <div className="w-20 h-20 rounded-2xl bg-red-300 bg-cover overflow-hidden">
+                <img
+                  src="https://i.ibb.co/CVsqkB8/Rectangle-146.png"
+                  alt=""
+                  className="hover:scale-110 duration-500 transition ease-in-out"
+                />
+              </div>
+              <div className="w-20 h-20 rounded-2xl bg-red-300 bg-cover overflow-hidden">
+                <img
+                  src="https://i.ibb.co/v46PrtS/Rectangle-147.png"
+                  alt=""
+                  className="hover:scale-110 duration-500 transition ease-in-out"
+                />
+              </div>
+              <div className="w-20 h-20 rounded-2xl bg-red-300 bg-cover overflow-hidden">
+                <img
+                  src="https://i.ibb.co/Js3dxDh/Rectangle-148.png"
+                  alt=""
+                  className="hover:scale-110 duration-500 transition ease-in-out"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="lg:w-[40%] mt-80 md:mt-32 lg:mt-0 h-full bg-fuchsia-00 flex flex-col items-start justify-center">
+          <p className="text-xl md:text-3xl font-bold">Add video, music, or image</p>
+          <p className="text-lg md:text-xl mt-4">
+            A voice maker that allows you to upload your creatives and sync it
+            with the voice of your choice.
+          </p>
+          <div className="flex items-center justify-center mt-10">
+            <div className="w-10 h-[2px] bg-custom-orange"></div>
+            <div className="-ml-4 text-3xl text-custom-orange">
+              <MdKeyboardArrowRight />
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="h-[30rem] w-full flex-col-reverse lg:flex-row mt-[13rem] md:mt-44 lg:mt-0 bg-green-00 flex">
+        <div className="lg:w-[40%] -mt-20 md:mt-0 h-full bg-fuchsia-00 flex flex-col items-start justify-center">
+          <p className="text-xl md:text-3xl font-bold">A vast library of voices</p>
+          <p className="text-lg md:text-xl mt-4">
+            Choose from over 120+ text to speech voices in 20+ languages
+          </p>
+          <div className="flex items-center justify-center mt-10">
+            <div className="w-10 h-[2px] bg-custom-orange"></div>
+            <div className="-ml-4 text-3xl text-custom-orange">
+              <MdKeyboardArrowRight />
+            </div>
+          </div>
+        </div>
+
+        <div className="lg:w-[60%] h-full bg-green-00 flex items-center p-36 md:p-0 -ml-40 md:ml-0 md:px-10 gap-2">
+            <div className="w-[40%] h-56 md:h-60 bg-orange-200 flex items-center rounded-s-2xl">
+                <div className="md:w-72 w-44 -mt-8 md:mt-0 h-40 bg-blue-30 absolute">
+                <div className="w-full h-full rounded-2xl flex flex-wrap pl-5 gap-2">
+
+                <div className="md:w-20 w-14  md:h-20 h-14 rounded-full bg-white p-2 bg-cover overflow-hidden">
+                <img
+                  src="https://i.ibb.co/v46PrtS/Rectangle-147.png"
+                  alt=""
+                  className="hover:scale-110 rounded-full duration-500 transition ease-in-out"
+                />
+              </div>
+
+              <div className="md:w-20 w-14  md:h-20 h-14 rounded-full bg-white p-2 bg-cover overflow-hidden">
+                <img
+                  src="https://i.ibb.co/v46PrtS/Rectangle-147.png"
+                  alt=""
+                  className="hover:scale-110 rounded-full duration-500 transition ease-in-out"
+                />
+              </div>
+
+              <div className="md:w-20 w-14  md:h-20 h-14 rounded-full bg-white p-2 bg-cover overflow-hidden">
+                <img
+                  src="https://i.ibb.co/v46PrtS/Rectangle-147.png"
+                  alt=""
+                  className="hover:scale-110 rounded-full duration-500 transition ease-in-out"
+                />
+              </div>
+
+              <div className="md:w-20 w-14  md:h-20 h-14 rounded-full bg-white p-2 bg-cover overflow-hidden">
+                <img
+                  src="https://i.ibb.co/v46PrtS/Rectangle-147.png"
+                  alt=""
+                  className="hover:scale-110 rounded-full duration-500 transition ease-in-out"
+                />
+              </div>
+
+              <div className="md:w-20 w-14  md:h-20 h-14 rounded-full bg-white p-2 bg-cover overflow-hidden">
+                <img
+                  src="https://i.ibb.co/v46PrtS/Rectangle-147.png"
+                  alt=""
+                  className="hover:scale-110 rounded-full duration-500 transition ease-in-out"
+                />
+              </div>
+
+              <div className="md:w-20 w-14  md:h-20 h-14 rounded-full bg-white p-2 bg-cover overflow-hidden">
+                <img
+                  src="https://i.ibb.co/v46PrtS/Rectangle-147.png"
+                  alt=""
+                  className="hover:scale-110 rounded-full duration-500 transition ease-in-out"
+                />
+              </div>
+
+                </div>
+                </div>
+            </div>
+            <div className="w-[60%] h- bg-red-00">
+            <div className="h-60 lg:h-80 aspect-video bg-white shadow-md w-72 lg:w-full rounded-md">
+          <div className="h-[79%] w-full flex items-center justify-center ">
+            <img
+              src="https://i.ibb.co/zNpb30y/Rectangle-9.png"
+              className="object-cover w-full -mt-8 md:-mt-8 lg:-mt-14 xl:-mt-14"
+              alt=""
+            />
+            <div className="w-10 h-10 rounded-full bg-white  absolute md:-mt-8 lg:-mt-10 xl:-mt-14 flex items-center justify-center text-gray-400">
+              <FaPlay />
+            </div>
+          </div>
+          <div className="h-[9%] px-10 ">
+            <div className="w-full h-[4px] bg-gray-400"></div>
+            <div className="w-16 md:w-20 lg:w-34 h-[4px] bg-custom-orange -mt-[4px]"></div>
+            <div className="w-16 md:w-20 lg:w-34 -mt-[4px] flex items-end justify-end">
+              <div className="w-3 h-3 rounded-full bg-custom-orange -mt-[4px]"></div>
+            </div>
+          </div>
+        </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import { Hero } from "../../components/Home/Hero";
 import { Data_Summary } from "../../components/Home/Data_Summary";
 import { Second_section } from "../../components/Home/Second_section";
 import { Third_section } from "../../components/Home/Third_section";
+import { Forth_Section } from "../../components/Home/Forth_Section";
 
 export const Home = () => {
   return <div className="flex flex-col justify-between items-center px-5 lg:px-20 ">
@@ -10,5 +11,6 @@ export const Home = () => {
     <Data_Summary/>
     <Second_section/>
     <Third_section/>
+    <Forth_Section/>
   </div>;
 };
